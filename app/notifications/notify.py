ae5c6b65b1da5cf3ad352943ae838177271d328b
Alexandre Decan <alexandre.decan@lexpage.net>
2015-03-21 13:34:03 +0100

    Correction d'un bug d'affichage suite à l'affichage d'une notification d'un message sur le minichat, contenant un lien.

--- app/notifications/notify.py
+++ app/notifications/notify.py
@@ -37,8 +37,8 @@ def notify(recipients, title, description, action, app, key):
             Notification.objects.get(app=app, key=key, recipient=recipient)
         except Notification.DoesNotExist as e: 
             # Create a new notification and save it
-            n = Notification(title=title, description=description, action=action, 
-                         recipient=recipient, app=app, key=key)
+            n = Notification(title=title, description=description, action=action,
+                             recipient=recipient, app=app, key=key)
             nb += 1
             n.save()
     return nb
@@ -53,7 +53,8 @@ def blog_draft_new(user, post):
     Send a notification to every user in BlogTeam. 
     """
     recipients = User.objects.filter(groups__name='BlogTeam')
-    notify(recipients, 'Un billet est en attente de validation', 'Le billet <em>%s</em> proposé par %s est en attente de validation.' % (escape(post.title), post.author),
+    notify(recipients, 'Un billet est en attente de validation',
+           'Le billet <em>%s</em> proposé par %s est en attente de validation.' % (escape(post.title), post.author),
            reverse('blog_pending_edit', kwargs={'pk': post.pk}), 'blog', 'pending-%d' % post.pk)
 
 
@@ -70,8 +71,10 @@ def blog_pending_approve(user, post):
     """
     blog_pending_clean(user, post)
     if post.author != user:
-        notify(post.author, 'Votre billet a été accepté', 'Le billet <em>%s</em> que vous avez proposé a été accepté par %s et sera prochainement publié.' % (escape(post.title), user.get_username()),
-           None, 'blog', 'validate-%d' % post.pk)
+        notify(post.author, 'Votre billet a été accepté',
+               'Le billet <em>%s</em> que vous avez proposé a été accepté par %s et sera prochainement publié.'
+               % (escape(post.title), user.get_username()),
+               None, 'blog', 'validate-%d' % post.pk)
 
 
 def blog_pending_delete(user, post):
@@ -80,8 +83,10 @@ def blog_pending_delete(user, post):
     """
     blog_pending_clean(user, post)
     if post.author != user:
-        notify(post.author, 'Votre billet a été refusé', 'Le billet <em>%s</em> que vous avez proposé a été refusé par %s.' % (escape(post.title), user.get_username()),
-           None, 'blog', 'validate-%d' % post.pk)    
+        notify(post.author, 'Votre billet a été refusé',
+               'Le billet <em>%s</em> que vous avez proposé a été refusé par %s.'
+               % (escape(post.title), user.get_username()),
+               None, 'blog', 'validate-%d' % post.pk)
 
 
 def messaging_thread_new(user, thread):
@@ -91,7 +96,9 @@ def messaging_thread_new(user, thread):
     """
     recipients = thread.recipients
     recipients.remove(user)
-    notify(recipients, 'Nouvelle conversation', '%s a entamé une nouvelle conversation avec vous : <em>%s</em>.' % (user.get_username(), escape(thread.title)),
+    notify(recipients, 'Nouvelle conversation',
+           '%s a entamé une nouvelle conversation avec vous : <em>%s</em>.'
+           % (user.get_username(), escape(thread.title)),
            reverse('messaging_show', kwargs={'thread': thread.pk}), 'messaging', 'thread-%d' % thread.pk)
 
 
@@ -102,7 +109,9 @@ def messaging_mesage_new(user, thread):
     """
     recipients = thread.recipients
     recipients.remove(user)
-    notify(recipients, 'Nouveau message', '%s a posté un nouveau message dans la conversation <em>%s</em>.' % (user.get_username(), escape(thread.title)),
+    notify(recipients, 'Nouveau message',
+           '%s a posté un nouveau message dans la conversation <em>%s</em>.'
+           % (user.get_username(), escape(thread.title)),
            reverse('messaging_show', kwargs={'thread': thread.pk})+'#unread', 'messaging', 'thread-%d' % thread.pk)
 
 
@@ -110,7 +119,9 @@ def board_post_moderate(user, message):
     """
     Notify the user that his post has been moderated. 
     """
-    notify(message.author, 'Message modéré', 'L\'un de vos messages a été modéré par %s dans la discussion <em>%s</em>.' % (user.get_username(), escape(message.thread.title)), 
+    notify(message.author, 'Message modéré',
+           'L\'un de vos messages a été modéré par %s dans la discussion <em>%s</em>.'
+           % (user.get_username(), escape(message.thread.title)),
            reverse('board_message_show', kwargs={'message': message.pk}), 'board', 'thread-%d' % message.thread.pk)
 
 
@@ -118,7 +129,8 @@ def profile_new(user):
     """ 
     Send a notification to welcome a newly created user.
     """
-    notify(user, 'Bienvenue sur Lexpage', 'Bienvenue sur Lexpage. Pensez à compléter votre profil et à choisir un avatar !',
+    notify(user, 'Bienvenue sur Lexpage',
+           'Bienvenue sur Lexpage. Pensez à compléter votre profil et à choisir un avatar !',
            reverse('profile_edit'), 'profile', 'new')
 
 
@@ -128,18 +140,25 @@ def slogan_new(user, slogan):
     that a new slogan has been posted. 
     """
     recipients = User.objects.filter(groups__name='SloganTeam')
-    notify(recipients, 'Nouveau slogan', 'Le slogan <em>%s</em> a été proposé par %s et doit être validé pour être visible.' % (escape(slogan.slogan), user.get_username()),
+    notify(recipients, 'Nouveau slogan',
+           'Le slogan <em>%s</em> a été proposé par %s et doit être validé pour être visible.'
+           % (escape(slogan.slogan), user.get_username()),
            reverse('admin:slogan_slogan_changelist'), 'slogan', slogan.pk)
+
 
 def minichat_warn(user, message):
     """
     Send a notification to target user to warn him that someone has 
     said his name in the minichat. 
     """
-
     year = message.date.year
     month = message.date.month
     dayhourminute = message.date.strftime('%d%H%M')
-    texte = smiley(urlize3(message.text))
-    notify(user, 'Minichat', '%s vous a adressé un message : <br/><em>%s</em>' % (message.user, texte),
-           reverse('minichat_archives', kwargs={'year':year, 'month':month})+('#%s%s%s' % (dayhourminute[0:2], dayhourminute[2:4], dayhourminute[4:6])), 'minichat', message.pk)
+    # The following line is commented because it leads to a tiny bug. Indeed, len(urlize3(text) could be greater
+    # than len(text), and sometimes greater than 255 chars which is a pain for the field in db and possibly leads to
+    # a truncated <a> when displayed.
+    # text = smiley(urlize3(message.text))
+    text = message.text
+    notify(user, 'Minichat', '%s vous a adressé un message : <br/><em>%s</em>' % (message.user, text),
+           reverse('minichat_archives', kwargs={'year': year, 'month': month})
+           + ('#%s%s%s' % (dayhourminute[0:2], dayhourminute[2:4], dayhourminute[4:6])), 'minichat', message.pk)
