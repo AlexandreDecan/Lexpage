4891f7b235462c2e0be6479e97131e1f5c7d7421
Alexandre Decan <alexandre.decan@lexpage.net>
2015-11-05 16:38:47 +0100

    Move PyMySQL default loading

--- app/settings.py
+++ app/settings.py
@@ -2,6 +2,3 @@
 
 # Default from dev environment
 from settings_dev import *
-
-import pymysql
-pymysql.install_as_MySQLdb()
--- app/settings_base.py
+++ app/settings_base.py
@@ -3,6 +3,11 @@
 import os
 
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Additional directories for fixtures
