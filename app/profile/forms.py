b21a80bce28c242005592cf3618b169dbf51df9c
Alexandre Decan <alexandre.decan@lexpage.net>
2015-11-11 14:58:46 +0100

    Workaround to fix #25

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -8,7 +8,8 @@ from .models import Profile
 from captcha.fields import ReCaptchaField
 
 import datetime
-
+import struct
+import imghdr
 
 user_fields = ['first_name', 'last_name', 'email']
 profile_fields = ['gender', 'birthdate', 'country', 'city', 'website_name', 'website_url', 'avatar',]
@@ -32,19 +33,18 @@ class MyPasswordSetForm(SetPasswordForm):
                                     min_length=8)
 
 
-
 class ChangeProfileForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=False, label='Prénom')
     last_name = forms.CharField(max_length=30, required=False, label='Nom',
                                 help_text='Cette information ne sera pas affichée publiquement.')
-    email = forms.EmailField(label='Adresse e-mail', required=True, 
+    email = forms.EmailField(label='Adresse e-mail', required=True,
                              help_text='Cette information ne sera pas affichée publiquement.')
 
-    birthdate = forms.DateField(required=False, label='Date de naissance', 
+    birthdate = forms.DateField(required=False, label='Date de naissance',
                                 widget=DateTimePicker(options={'format': 'DD/MM/YYYY', 'pickTime': False}))
 
-    avatar_file = forms.ImageField(required=False, label='Envoyer un avatar depuis votre disque',
-                                   help_text='Vous pouvez envoyer un avatar depuis votre disque. Il doit s\'agir d\'un fichier image, en .gif, .jpg ou .png et de moins de 120x120 pixels. Votre ancien avatar sera automatiquement effacé et remplacé par le nouvel avatar.')
+    avatar_file = forms.FileField(required=False, label='Envoyer un avatar depuis votre disque',
+                                   help_text='Vous pouvez envoyer un avatar depuis votre disque. Il doit s\'agir d\'un fichier image, en .gif, .jpg ou .png. Votre ancien avatar sera automatiquement effacé et remplacé par le nouvel avatar.')
 
     class Meta:
         model = Profile
@@ -60,12 +60,14 @@ class ChangeProfileForm(forms.ModelForm):
         except IndexError:
             extension = None
 
-        if not(extension in ['jpg', 'gif', 'png']):
-            raise forms.ValidationError('Les seules extensions autorisées sont .jpg, .png et .gif.')
+        if not(extension in ['jpg', 'jpeg', 'gif', 'png']):
+            raise forms.ValidationError('Les seules extensions autorisées sont .jpg, .jpeg, .png et .gif.')
         else:
             self.cleaned_data['avatar_file_ext'] = extension
 
-        # TODO: Look for an alternative for PIL/Pillow that does not require external libs
+        if in_file.size > 512 * 1024:
+            raise forms.ValidationError('L\'image doit être inférieure à 512ko.')
+
         return self.cleaned_data['avatar_file']
 
         # from PIL import Image, ImageFileIO
@@ -90,7 +92,7 @@ class ChangeProfileForm(forms.ModelForm):
 
 class LoginForm(AuthenticationForm):
     remember_me = forms.BooleanField(initial=True, required=False, label='Rester connecté')
-    
+
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update({'class': 'username_complete'})
@@ -99,7 +101,7 @@ class LoginForm(AuthenticationForm):
 class RegistrationForm(forms.Form):
     username = forms.RegexField(regex=r'^[\w.@+_-]+$',
                                 min_length=3,
-                                max_length=20, 
+                                max_length=20,
                                 required=True,
                                 label='Nom d\'utilisateur',
                                 error_messages={'invalid': 'Ce champ ne peut contenir que des lettres, des nombres et les caractères @/./+/-/_.'})
@@ -107,7 +109,7 @@ class RegistrationForm(forms.Form):
                              required=True)
     password1 = forms.CharField(widget=forms.PasswordInput,
                                 required=True,
-                                label='Mot de passe', 
+                                label='Mot de passe',
                                 min_length=8)
     password2 = forms.CharField(widget=forms.PasswordInput,
                                 required=True,
