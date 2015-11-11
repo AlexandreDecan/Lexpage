d77bd9129d5e9c29e152f405a83f1ec104cf92c2
Alexandre Decan <alexandre.decan@lexpage.net>
2015-11-11 14:34:39 +0100

    Fix #17 : The first post of a day will be published at midnight, regardless of the number of available posts.

--- app/blog/management/commands/publish_next_post.py
+++ app/blog/management/commands/publish_next_post.py
@@ -1,65 +1,48 @@
 from django.core.management.base import NoArgsCommand
-
 from blog.models import BlogPost
-
 import datetime
-
-"""
-class Command(NoArgsCommand):
-    help = "Publish the first available blog entry. "
-
-    def handle_noargs(self, **options):
-        try:
-            post = BlogPost.approved.all()[0]
-            post.change_status(None, BlogPost.STATUS_PUBLISHED)
-            return 'publish: %s' % post.title
-        except IndexError:
-            return 'No blog entry available!?'
-"""
-
 
 class Command(NoArgsCommand):
     help = "Publish the first available blog entry. "
 
     # (min available, nb per day)
-    number_per_day = [(8, 2), (1, 1)]
+    number_per_day = [(8, 2), (1, 1), (0, 0)]
     # number_per_day = [(1, 2), ]
 
     def handle_noargs(self, **options):
+        today = datetime.date.today()
+
+        # Do not publish on sunday and in public holiday
+        if today.weekday() == 6:
+            return 'No post on sunday'
+        if (today.day, today.month) in [(1, 1), (1, 5), (14, 7), (21, 7), (15, 8), (1, 11), (11, 11), (25, 12)]:
+            return 'No post on public holiday'
+
+        # Number of already published for today
+        published = len(BlogPost.published.filter(date_published__gte=today))
+
+        # Number of available posts for today
+        available = len(BlogPost.approved.all())
+        if available == 0:
+            return 'No available post'
+
+        # Expected number of posts for today
+        expected = list(filter(lambda x: (published + available) >= x[0], Command.number_per_day))[0][1]
+        if published == expected:
+            return None
+
+        # Time between each post for today
+        delay = (24 * 3600) / expected
+
+        # Time of latest published post today
         try:
-            # If it's sunday, dismiss
-            if datetime.datetime.now().weekday() == 6:
-                return None
+            latest_post = BlogPost.published.filter(date_published__gte=today).order_by('date_published').last()
+            latest_delay = (datetime.datetime.now() - latest_post.date_published).total_seconds()
+        except Exception:
+            latest_delay = 10 * delay  # Ensure a new publication
 
-            # Dismiss in case of public holiday
-            day, month = (datetime.date.today().day, datetime.date.today().month)
-            if (day, month) in [(1, 1), (1, 5), (14, 7), (21, 7), (15, 8), (1, 11), (11, 11), (25, 12)]:
-                return 'Public holiday'
-
-            # Get number of available posts
-            posts = BlogPost.approved.all()
-            nb_posts = len(posts)
-            if nb_posts == 0:
-                # No post? no post!
-                return 'No available post'
-
-            # nb_per_day contains the number of posts per day wrt the stock
-            nb_per_day = 0
-            for min_post, nb_post in Command.number_per_day:
-                if nb_posts >= min_post:
-                    nb_per_day = nb_post
-                    break
-
-            # Time since last published post, in hours
-            delta = (datetime.datetime.now() - BlogPost.published.latest().date_published)
-            delta = (delta.seconds + delta.days * 24 * 3600) / 3600
-            delta += 0.5  # Dismiss OVH delay
-
-            # If delta (in hour) > time_frame, publish!
-            if delta >= (24 / nb_per_day):
-                posts[0].change_status(None, BlogPost.STATUS_PUBLISHED)
-                return 'Task: publish "%s"' % str(posts[0].title)
-            else:
-                return None
-        except Exception as e: 
-            return str(e)
+        # Do we publish?
+        if latest_delay + 600 >= delay:  # Add 10 minutes to latest_delay as cron job are not very accurate
+            post = BlogPost.approved.first()
+            # post.change_status(None, BlogPost.STATUS_PUBLISHED)
+            return 'Publish %s' % post.title
