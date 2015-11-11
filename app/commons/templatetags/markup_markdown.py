2e0bc1f3705d64ea4ca21041d0668394b4e05037
Alexandre Decan <alexandre.decan@lexpage.net>
2015-11-11 13:17:48 +0100

    Rely on an external library for markdown, and integrate our OEmbed extension into (fixes #27)

--- app/commons/templatetags/markup_markdown.py
+++ app/commons/templatetags/markup_markdown.py
@@ -1,29 +1,34 @@
 from django import template
 from django.utils.safestring import mark_safe
-from django.utils.encoding import force_text
 
-from . import markdown
-
+import markdown
+from markdown.inlinepatterns import LinkPattern
 
 register = template.Library()
 
 
-from .markdown.extensions import def_list, fenced_code, footnotes, tables, \
-                                nl2br, smart_strong, sane_lists, lexpage_oembed
+class OEmbedExtension(markdown.Extension):
+    class OEmbedPattern(LinkPattern):
+        def handleMatch(self, m):
+            el = markdown.util.etree.Element('a')
+            el.text = m.group(2)
+            href = m.group(2)
 
-extensions = [def_list.makeExtension(), 
-                fenced_code.makeExtension(),
-                footnotes.makeExtension(),
-                tables.makeExtension(),
-                nl2br.makeExtension(),
-                smart_strong.makeExtension(),
-                sane_lists.makeExtension(),
-                lexpage_oembed.makeExtension()]
+            el.set('href', self.sanitize_url(self.unescape(href.strip())))
+            el.set('class', 'oembed')
+
+            return el
+
+    def extendMarkdown(self, md, md_globals):
+        """ Modify inline patterns. """
+        md.inlinePatterns.add('embed', OEmbedExtension.OEmbedPattern('\[!embed\]\((.*)\)', md), '<link')
 
 
 @register.filter(name='markdown', is_safe=True)
 def __markdown(value):
-    # value = force_text(value)
+    extensions = ['def_list', 'fenced_code', 'footnotes', 'tables', 'nl2br',
+                  'smart_strong', 'sane_lists', OEmbedExtension()]
+
     value = markdown.markdown(value, safe_mode='escape', output_format='html5', lazy_ol=False, extensions=extensions)
 
     return mark_safe(value)
