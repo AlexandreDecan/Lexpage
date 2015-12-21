c281e9d517deb036cb54b918bcafe32b8226303b
Julien Pivotto <roidelapluie@inuits.eu>
2015-12-21 08:53:08 +0100

    Start working on Micawber integration

--- app/views.py
+++ app/views.py
@@ -5,6 +5,11 @@ from django.template import RequestContext
 
 from blog.models import BlogPost
 from board.models import Thread
+from django.conf import settings
+
+import base64
+import hashlib
+import micawber
 
 import datetime
 
@@ -14,9 +19,9 @@ HOMEPAGE_THREAD_DELAY = 3   # Number of days before expiration
 
 
 def homepage(request):
-    """ 
-    Simple view corresponding to the homepage. It provides some blog posts 
-    and some board posts, including annotated threads). 
+    """
+    Simple view corresponding to the homepage. It provides some blog posts
+    and some board posts, including annotated threads).
     """
     context = {}
 
@@ -33,6 +38,26 @@ def homepage(request):
         thread.annotate_flag(request.user)
     context['thread_list'] = threads
 
-    return render_to_response('homepage.html', 
-                              context, 
+    return render_to_response('homepage.html',
+                              context,
                               context_instance=RequestContext(request))
+
+def render_rich_content(request, url_checksum, url_b64):
+    """ Retrieve rich text content.
+    Params:
+        url_checksum: md5sum(settings.RICH_TEXT_CHECKSUM, url)
+        url_b64: b64 of the URL
+
+    url_checksum should prevent malicous usage to crawl the web."""
+    m = hashlib.md5()
+    m.update(bytes(settings.RICH_TEXT_CONTENT_SALT))
+    m.update(bytes(url_b64))
+    computed_checksum = m.digest()
+    if computed_checksum != url_checksum:
+        raise ValueError
+
+    real_url = base64.b64decode(url_b64)
+    provider = micawber.bootstrap_basic()
+    provider.request(real_url)
+
+
