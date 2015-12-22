958cd7ac882a87db1143077f24ac282ae629b0ba
Alexandre Decan <alexandre.decan@lexpage.net>
2015-12-22 14:37:20 +0100

    Remove whitenoise (already removed from Django, but not from the wsgi app)

--- app/wsgi.py
+++ app/wsgi.py
@@ -4,9 +4,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-
-# Serve static files using Python, for testing purposes
-application = DjangoWhiteNoise(application)
