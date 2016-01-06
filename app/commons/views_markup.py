cbb2888da98e4323ba04add1bffb5a3b0c6c3ad4
Alexandre Decan <alexandre.decan@lexpage.net>
2016-01-06 10:26:42 +0100

    CSRF is not needed for markup preview since we inject it for each ajax POST call

--- app/commons/views_markup.py
+++ app/commons/views_markup.py
@@ -1,14 +1,12 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 
 
 class MarkupPreviewView(TemplateView):
     template_name = 'commons/markup_preview.html'
 
     @method_decorator(login_required)
-    @method_decorator(csrf_exempt)
     def post(self, request, *args, **kwargs):
         context = super(MarkupPreviewView, self).get_context_data(**kwargs)
 
