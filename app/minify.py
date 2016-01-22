7b2077340eb756b7992fb9e63c211a2114741f75
Julien Pivotto <roidelapluie@inuits.eu>
2016-01-22 23:07:30 +0100

    Use YUI Compressor to minify

--- app/minify.py
+++ app/minify.py
@@ -1,44 +1,40 @@
 from django.contrib.staticfiles.storage import StaticFilesStorage
-from django.core.files.base import File, ContentFile
+from django.core.files.base import ContentFile
 from django.conf import settings
 from django.utils.deconstruct import deconstructible
 
-import sys
-import traceback
+from tempfile import NamedTemporaryFile
+from yuicompressor import run
 
-import slimit
-import csscompressor
-
+import logging
 
 @deconstructible
 class MinifyStatic(StaticFilesStorage):
+    supported_suffixes = ('js', 'css',)
+
     def __init__(self, option=None):
         super().__init__()
-        self._minify_js = getattr(settings, 'MINIFY_JS', False)
-        self._minify_css = getattr(settings, 'MINIFY_CSS', False)
         self._ignored_prefixes = getattr(settings, 'MINIFY_IGNORED_PATHS', [])
 
+    def minify_enabled(self, kind):
+        return getattr(settings, 'MINIFY_%s' % kind.upper(), False)
+
     def _save(self, name, original_file):
-        if any((name.startswith(prefix) for prefix in self._ignored_prefixes)):
-            file = original_file
-        elif self._minify_js and name.endswith('.js') and not name.endswith('.min.js'):
-            try:
-                new_content = slimit.minify(original_file.read().decode('utf-8'), mangle=True, mangle_toplevel=True)
-                file = ContentFile(new_content)
-            except Exception as e:
-                print('Minify JS error for {}:\n{}'.format(name, e), file=sys.stderr)
-                traceback.print_exc(file=sys.stderr)
-                file = original_file
-
-        elif self._minify_css and name.endswith('.css') and not name.endswith('.min.css'):
-            try:
-                new_content = csscompressor.compress(original_file.read().decode('utf-8'))
-                file = ContentFile(new_content)
-            except Exception as e:
-                print('Minify CSS error for {}:\n{}'.format(name, e), file=sys.stderr)
-                traceback.print_exc(file=sys.stderr)
-                file = original_file
-        else:
-            file = original_file
-
+        file = original_file
+        if not any((name.startswith(prefix) for prefix in self._ignored_prefixes)):
+            for suffix in self.supported_suffixes:
+                if self.minify_enabled(suffix) and name.endswith('.%s' % suffix) \
+                and not name.endswith('.min.%s' % suffix):
+                    try:
+                        full_content = NamedTemporaryFile(suffix='.%s' % suffix)
+                        full_content.write(original_file.read())
+                        full_content.flush()
+                        minified_content = NamedTemporaryFile(suffix='.min.%s' % suffix)
+                        yuicompress = run(full_content.name, '-o', minified_content.name)
+                        assert yuicompress == 0, 'YUI Compressor failed on %s' % name
+                        new_content = minified_content.read()
+                        file = ContentFile(new_content)
+                    except Exception:
+                        logging.exception('Minify %s error for %s' % (suffix, name))
+                        file = original_file
         return super()._save(name, file)
