4a28c250aedd1ddaa6f7f1a8d7381492609fb92f
Alexandre Decan <alexandre.decan@lexpage.net>
2016-01-22 10:57:13 +0100

    Add an wsgi_ws.py specific wsgi app.

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,6 +1,5 @@
 import os
 
-# Environment variable must be defined before importing whitenoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
---
+++ app/wsgi_ws.py
@@ -0,0 +1,10 @@
+import os
+import gevent.socket
+import redis.connection
+redis.connection.socket = gevent.socket
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
+
+application = uWSGIWebsocketServer()
