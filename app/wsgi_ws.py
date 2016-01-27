12c9fc709c17dcceab15f3c70fd78766034c909a
Alexandre Decan <alexandre.decan@lexpage.net>
2016-01-27 17:35:43 +0100

    Use gevent.monkey.patch_thread() in wsgi_ws

--- app/wsgi_ws.py
+++ app/wsgi_ws.py
@@ -1,6 +1,9 @@
 import os
 import gevent.socket
+import gevent.monkey
 import redis.connection
+
+gevent.monkey.patch_thread()
 redis.connection.socket = gevent.socket
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
@@ -8,3 +11,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 
 application = uWSGIWebsocketServer()
+
+
