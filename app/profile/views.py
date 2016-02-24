14513919025fb5032beba7f6825c4124b087b425
Alexandre Decan <alexandre.decan@lexpage.net>
2016-02-24 09:33:22 +0100

    Properly redirect after login

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,7 +1,10 @@
 import os
+from django.core.exceptions import SuspiciousOperation
 from django.core.urlresolvers import reverse
 
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
@@ -11,7 +14,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.views import login as view_login
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, REDIRECT_FIELD_NAME
 
 from notifications.models import Notification
 
@@ -83,6 +86,17 @@ class LoginView(FormView):
     form_class = LoginForm
     template_name = 'profile/login.html'
     http_method_names = ['get', 'post']
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        redirect_to = self.request.GET.get(self.redirect_field_name, reverse(settings.LOGIN_REDIRECT_URL))
+        if not redirect_to.startswith('/'):
+            raise SuspiciousOperation()
+        context[self.redirect_field_name] = redirect_to
+
+        return context
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
