98b1886a27228bc8f9000c8ede6ac1e9c93b420c
Alexandre Decan <alexandre.decan@lexpage.net>
2017-04-21 09:54:12 +0200

    Switch from Embedly to Noembed

--- app/commons/static/js/scripts.js
+++ app/commons/static/js/scripts.js
@@ -11,19 +11,19 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 
 function refresh_oembed(target) {
     target.find('a.oembed').oembed(null, {maxHeight:"400"});
+
     target.find('span.embedly').each(function (i, e) {
         e = $(e);
-        var README = "Please, do NOT use this API key. You can get one too for free on http://embed.ly !";
-        var url = "https://api.embed.ly/1/extract?key=396f02e230724c218db720b2dd2daed0&url="+e.attr('data-url');
-        $.ajax(url).done(function (r) {
-            if (r['images'] && r['images'][0] && r['images'][0]['url']) {
-                e.append('<a href="'+ e.attr('data-url')+ '"><img src="'+r['images'][0]['url']+'"/></a>');
+        var url = "https://noembed.com/embed?url=" + e.attr('data-url');
+
+        $.getJSON(url).done(function (r) {
+            if (r['thumbnail_url']) {
+               e.append('<a href="'+ e.attr('data-url')+ '"><img src="'+r['thumbnail_url']+'"/></a>');
             } else {
-                e.replaceWith('');
+               e.replaceWith('');
             }
         });
     });
-
 }
 
 function activate_tooltips(target) {
