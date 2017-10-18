426c56b23bb01afe565290a6e0b515d035f50694
Alexandre Decan <alexandre.decan@lexpage.net>
2017-10-18 13:41:45 +0200

    Prevent [!embed](x) to convert x using markdown

--- app/commons/templatetags/markup_markdown.py
+++ app/commons/templatetags/markup_markdown.py
@@ -2,19 +2,18 @@ from django import template
 from django.utils.safestring import mark_safe
 
 import markdown
-from markdown.inlinepatterns import LinkPattern
 
 register = template.Library()
 
 
 class OEmbedExtension(markdown.Extension):
-    class OEmbedPattern(LinkPattern):
+    class OEmbedPattern(markdown.inlinepatterns.LinkPattern):
         def handleMatch(self, m):
             el = markdown.util.etree.Element('a')
-            el.text = m.group(2)
-            href = m.group(2)
+            link = markdown.util.AtomicString(m.group(2))
 
-            el.set('href', self.sanitize_url(self.unescape(href.strip())))
+            el.text = link
+            el.set('href', self.sanitize_url(self.unescape(link)))
             el.set('class', 'oembed')
 
             return el
