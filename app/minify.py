ba18fc43f4d0ea5b99bbec965e18d180559e48dd
Alexandre Decan <alexandre.decan@lexpage.net>
2020-12-08 13:44:32 +0100

    Replace yuicompressor that disappeared from PyPI

--- app/minify.py
+++ app/minify.py
@@ -4,14 +4,13 @@ from django.conf import settings
 from django.utils.deconstruct import deconstructible
 
 from tempfile import NamedTemporaryFile
-from yuicompressor import run
+from css_html_js_minify import css_minify, js_minify
 
 import logging
 
 
 @deconstructible
 class MinifyStatic(StaticFilesStorage):
-    supported_suffixes = ('js', 'css',)
 
     def __init__(self, option=None):
         super().__init__()
@@ -23,19 +22,19 @@ class MinifyStatic(StaticFilesStorage):
     def _save(self, name, original_file):
         file = original_file
         if not any((name.startswith(prefix) for prefix in self._ignored_prefixes)):
-            for suffix in self.supported_suffixes:
-                if self.minify_enabled(suffix) and name.endswith('.%s' % suffix) \
-                        and not name.endswith('.min.%s' % suffix):
-                    try:
-                        full_content = NamedTemporaryFile(suffix='.%s' % suffix)
-                        full_content.write(original_file.read())
-                        full_content.flush()
-                        minified_content = NamedTemporaryFile(suffix='.min.%s' % suffix)
-                        yuicompress = run(full_content.name, '-o', minified_content.name)
-                        assert yuicompress == 0, 'YUI Compressor failed on %s' % name
-                        new_content = minified_content.read()
-                        file = ContentFile(new_content)
-                    except Exception:  # pragma: no cover
-                        logging.exception('Minify %s error for %s' % (suffix, name))
-                        file = original_file
+            if name.endswith('.js') and not name.endswith('.min.js') and self.minify_enabled('js'):
+                func = js_minify
+            elif name.endswith('.css') and not name.endswith('.min.css') and self.minify_enabled('css'):
+                func = css_minify
+            else:
+                func = None
+
+            if func is not None:
+                try:
+                    content = original_file.read()
+                    minified = func(content)
+                    file = ContentFile(minified)
+                except Exception:  # pragma: no cover
+                    logging.exception('Minify error for %s' % name,)
+                    file = original_file
         return super()._save(name, file)
